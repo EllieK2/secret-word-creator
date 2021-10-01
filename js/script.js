4e807f3e4ac24dfe07540ccbfecde968ab5379dd
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-01 21:08:09 +0100

    Created first variable and console.log

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
   // PLEASE ADD ALL YOUR CODE HERE
+
+var lowercaseArray = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+console.log (lowercaseArray) {}
 }
 
 // Write password to the #password input
