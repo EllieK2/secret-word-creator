f0f5ef76aebfc0a5eb6294b452d2c7e2d90caf89
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-01 21:14:35 +0100

    Created an arrey of uppercase characters

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ function generatePassword() {
   // PLEASE ADD ALL YOUR CODE HERE
 
 var lowercaseArray = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-console.log (lowercaseArray) {}
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+console.log(uppercaseArray){}
 }
 
 // Write password to the #password input
