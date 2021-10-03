35dc71d9a3c80e277969f457934d9ab51bb28f64
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-03 16:34:31 +0100

    changed var into const

--- js/script.js
+++ js/script.js
@@ -5,19 +5,23 @@ function generatePassword() {
   console.log ("hello world")
 
   // Arranged the criteria
-var lowercaseArray = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
-var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var specialCharacterArray = ["!","#", "$", "%", "&", "'", "(", ")", "*", "+",",", "-", ".","/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "'", "{", "|", "}", "~"]
+const lowercaseArray = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+const numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const specialCharacterArray = ["!","#", "$", "%", "&", "'", "(", ")", "*", "+",",", "-", ".","/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "'", "{", "|", "}", "~"]
 // Ask the user for the password (gathering an information) // prompt and confirm  
 // password length 8< 128
 // lowercase, uppercase, numbers, special characters,
-var numCharacter = prompt ("How many number of characters you would like between 8 and 128?")
-var numbers = confirm ("Would you like numbers in your password?")
-var uppercase = confirm ("Would you like Uppercase characters in your password?") 
-var lowercase = confirm ("Would you like Lowercase characters in your password")
-var characters = confirm ("Would you like special characters in your password?")
-return 
+const numCharacter = prompt ("How many character numbers would you like between 8 and 128?")
+const numbers = confirm ("Would you like numbers in your password?")
+const uppercase = confirm ("Would you like uppercase characters in your password?") 
+const lowercase = confirm ("Would you like lowercase characters in your password?")
+const characters = confirm ("Would you like special characters in your password?")
+
+//Validate user answers 
+
+
+
 // Generate a password (use loop) 
 
 // Return (generated password based on criteria)
