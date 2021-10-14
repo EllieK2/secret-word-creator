6e3ffcdcf33825a7dd4b92de099f872013536c53
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-14 21:51:28 +0100

    final changes before submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const specialCharacters = ["!","#", "$", "%", "&", "'", "(", ")", "*", "+",",", "-", ".","/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "'", "{", "|", "}", "~"];
 
 //define variables
-const numberSelection = false; 
-const upperSelection = false; 
-const lowerSelection = false; 
-const specialCharacterSelection = false; 
+///const numberSelection = false; 
+///const upperSelection = false; 
+///const lowerSelection = false; 
+///const specialCharacterSelection = false; 
 
 const generateBtn = document.querySelector('#generate');
 console.log('generate password'); 
@@ -34,37 +34,31 @@ passwordOptions.length = length
 return passwordOptions 
 };
 
-//function to define a password length 
+//function to define a password length  
 function getPasswordLength () {
   const length = prompt ("Choose password length between 8 and 128");
   return length; 
   
 };
 
+////broken code I dont seem to get this working from that point 
+
 //define character types to use for the password 
+//if (confirmLength) {
+//if (confirm ("Would you like to include numbers in your password?") === true) {
+ // numberCase = true
+ // }
 
-if (confirmLength) {
-  if (confirm ("Would you like to include numbers in your password?") === true) {
-    numberSelection = true
-  }
+//if (confirm ("Would you like to include uppercase characters in your password?") === true) {
+ // upperCase = true
+//}
 
-if (confirm ("Would you like to include uppercase characters in your password?") === true) {
-    upperSelection = true
-}
-
-if (confirm ("Would you like lowercase characters in your password?") === true) {
-  lowerSelection = true
-}
-if (confirm ("Would you like special characters in your password?") === true) { 
-  specialCharacterSelection = true 
-}
-///var responses = {
-    ///length: length,
-    ///askNumbers: askNumbers,
-    ///askLowerCase: askLowerCase,
-    ///askUpperCase: askUpperCase, 
-    ///askSpecialCharacters: askSpecialCharacters, 
-///}
+//if (confirm ("Would you like lowercase characters in your password?") === true) {
+ // lowerCase = true
+//}
+//if (confirm ("Would you like special characters in your password?") === true) { 
+ // specialCharacterSelectionCase = true 
+//}
 
 // Write password to the #password input
 function writePassword() {
