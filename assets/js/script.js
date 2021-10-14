97963b3406295cb1a22fa1ab578beaa02390eba2
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-14 14:18:08 +0100

    defined variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,77 @@
-console.log('hello')
-
-const generateBtn = document.querySelector('generate');
-console.log('generate password')
-
-function generatePassword() {
-
 const lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
 const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const specialCharacters = ["!","#", "$", "%", "&", "'", "(", ")", "*", "+",",", "-", ".","/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "'", "{", "|", "}", "~"];
 
-function questions () {
-const isValid = false; 
-do {
+//define variables
+const numberSelection = false; 
+const upperSelection = false; 
+const lowerSelection = false; 
+const specialCharacterSelection = false; 
 
-var length = prompt ("Choose password length between 8 and 128");
-var askNumbers = confirm ("Would you like to include numbers in your password?");
-var askUpperCase = confirm ("Would you like to include uppercase characters in your password?"); 
-var askLowerCase = confirm ("Would you like lowercase characters in your password?");
-var askSpecialCharacters = confirm ("Would you like special characters in your password?");
+const generateBtn = document.querySelector('#generate');
+console.log('generate password'); 
 
-var responses = {
-    length: length,
-    askNumbers: askNumbers,
-    askLowerCase: askLowerCase,
-    askUpperCase: askUpperCase, 
-    askSpecialCharacters: askSpecialCharacters, 
+function  generatePassword() {
+
+const length = getPasswordLength ()
+const askNumbers = confirm ("Would you like to include numbers in your password?");
+const askUpperCase = confirm ("Would you like to include uppercase characters in your password?"); 
+const askLowerCase = confirm ("Would you like lowercase characters in your password?");
+const askSpecialCharacters = confirm ("Would you like special characters in your password?");
+
+}
+// function to define criteria selected for the password
+function acceptanceCriteria () {
+
+const passwordOptions = {}
+const length = getPasswordLength ()
+const askNumbers = confirm ("Would you like to include numbers in your password?");
+const askUpperCase = confirm ("Would you like to include uppercase characters in your password?"); 
+const askLowerCase = confirm ("Would you like lowercase characters in your password?");
+const askSpecialCharacters = confirm ("Would you like special characters in your password?");
+passwordOptions.length = length 
+return passwordOptions 
+};
+
+//function to define a password length 
+function getPasswordLength () {
+  const length = prompt ("Choose password length between 8 and 128");
+  return length; 
+  
+};
+
+//define character types to use for the password 
+
+if (confirmLength) {
+  if (confirm ("Would you like to include numbers in your password?") === true) {
+    numberSelection = true
+  }
+
+if (confirm ("Would you like to include uppercase characters in your password?") === true) {
+    upperSelection = true
 }
 
-
-
-
+if (confirm ("Would you like lowercase characters in your password?") === true) {
+  lowerSelection = true
+}
+if (confirm ("Would you like special characters in your password?") === true) { 
+  specialCharacterSelection = true 
+}
+///var responses = {
+    ///length: length,
+    ///askNumbers: askNumbers,
+    ///askLowerCase: askLowerCase,
+    ///askUpperCase: askUpperCase, 
+    ///askSpecialCharacters: askSpecialCharacters, 
+///}
 
 // Write password to the #password input
-///function writePassword() {
-   /// var password = generatePassword();
-    ///var passwordText = document.querySelector('#password');
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector('#password');
   
-    ///passwordText.value = password;
-  ///}
-  
-  // Add event listener to generate button
-  ///generateBtn.addEventListener('click', writePassword);
-  
+passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
